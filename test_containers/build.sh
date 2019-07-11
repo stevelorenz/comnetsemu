1f6cede9222c2fd4203b35e2ba3d467431e18611
Simon Hanisch <simon.hanisch@tu-dresden.de>
2019-07-11 17:56:00 +0200

    First version of the packet filter exercise

--- test_containers/build.sh
+++ test_containers/build.sh
@@ -6,3 +6,4 @@
 sudo docker build -t dev_test -f ./Dockerfile.dev_test .
 sudo docker build -t alpine_dockerhost -f ./Dockerfile.alpine_dockerhost .
 sudo docker build -t sec_test -f ./Dockerfile.sec_test .
+sudo docker build -t nginx -f ./Dockerfile.nginx .
