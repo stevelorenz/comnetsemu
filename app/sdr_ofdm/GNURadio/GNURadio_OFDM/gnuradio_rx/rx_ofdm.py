9f960d8918abef5dacf3446f35dd249f96302e74
Javier Acevedo <s0505752@tu-dresden.de>
2019-08-07 13:17:16 +0200

    Update README and add new OFDM files

---
+++ app/sdr_ofdm/GNURadio/GNURadio_OFDM/gnuradio_rx/rx_ofdm.py
@@ -0,0 +1,208 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+##################################################
+# GNU Radio Python Flow Graph
+# Title: Rx Ofdm
+# GNU Radio version: 3.7.13.5
+##################################################
+
+if __name__ == '__main__':
+    import ctypes
+    import sys
+    if sys.platform.startswith('linux'):
+        try:
+            x11 = ctypes.cdll.LoadLibrary('libX11.so')
+            x11.XInitThreads()
+        except:
+            print "Warning: failed to XInitThreads()"
+
+from PyQt4 import Qt
+from gnuradio import blocks
+from gnuradio import digital
+from gnuradio import eng_notation
+from gnuradio import filter
+from gnuradio import gr
+from gnuradio import qtgui
+from gnuradio import uhd
+from gnuradio.eng_option import eng_option
+from gnuradio.filter import firdes
+from optparse import OptionParser
+import sip
+import sys
+import time
+from gnuradio import qtgui
+
+
+class rx_ofdm(gr.top_block, Qt.QWidget):
+
+    def __init__(self):
+        gr.top_block.__init__(self, "Rx Ofdm")
+        Qt.QWidget.__init__(self)
+        self.setWindowTitle("Rx Ofdm")
+        qtgui.util.check_set_qss()
+        try:
+            self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
+        except:
+            pass
+        self.top_scroll_layout = Qt.QVBoxLayout()
+        self.setLayout(self.top_scroll_layout)
+        self.top_scroll = Qt.QScrollArea()
+        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
+        self.top_scroll_layout.addWidget(self.top_scroll)
+        self.top_scroll.setWidgetResizable(True)
+        self.top_widget = Qt.QWidget()
+        self.top_scroll.setWidget(self.top_widget)
+        self.top_layout = Qt.QVBoxLayout(self.top_widget)
+        self.top_grid_layout = Qt.QGridLayout()
+        self.top_layout.addLayout(self.top_grid_layout)
+
+        self.settings = Qt.QSettings("GNU Radio", "rx_ofdm")
+        self.restoreGeometry(self.settings.value("geometry").toByteArray())
+
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.tx_gain = tx_gain = 60
+        self.samp_rate = samp_rate = 500e3
+        self.packet_len = packet_len = 60
+        self.len_tag_key = len_tag_key = "packet_len"
+        self.freq = freq = 2.4e9
+        self.fft_len = fft_len = 120
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self.uhd_usrp_source_0 = uhd.usrp_source(
+        	",".join(('addr=192.168.10.2', "")),
+        	uhd.stream_args(
+        		cpu_format="fc32",
+        		channels=range(1),
+        	),
+        )
+        self.uhd_usrp_source_0.set_samp_rate(samp_rate)
+        self.uhd_usrp_source_0.set_center_freq(2.4e9, 0)
+        self.uhd_usrp_source_0.set_gain(0, 0)
+        self.uhd_usrp_source_0.set_antenna('RX2', 0)
+        self.uhd_usrp_source_0.set_auto_dc_offset(True, 0)
+        self.uhd_usrp_source_0.set_auto_iq_balance(True, 0)
+        self.rational_resampler_xxx_0_0 = filter.rational_resampler_ccc(
+                interpolation=1,
+                decimation=2,
+                taps=None,
+                fractional_bw=None,
+        )
+        self.qtgui_sink_x_0 = qtgui.sink_f(
+        	1024, #fftsize
+        	firdes.WIN_BLACKMAN_hARRIS, #wintype
+        	2.4e9, #fc
+        	samp_rate, #bw
+        	"", #name
+        	True, #plotfreq
+        	True, #plotwaterfall
+        	False, #plottime
+        	False, #plotconst
+        )
+        self.qtgui_sink_x_0.set_update_time(1.0/10)
+        self._qtgui_sink_x_0_win = sip.wrapinstance(self.qtgui_sink_x_0.pyqwidget(), Qt.QWidget)
+        self.top_grid_layout.addWidget(self._qtgui_sink_x_0_win)
+
+        self.qtgui_sink_x_0.enable_rf_freq(True)
+
+        def qtgui_sink_x_0_callback(p, num):
+        	if num == 1 or num == 2: self.set_freq(p.x())
+
+        Qt.QObject.connect(self._qtgui_sink_x_0_win, Qt.SIGNAL("plotPointSelected(QPointF, int)"), qtgui_sink_x_0_callback )
+
+
+        self.digital_ofdm_rx_0 = digital.ofdm_rx(
+        	  fft_len=fft_len, cp_len=32,
+        	  frame_length_tag_key='frame_'+len_tag_key,
+        	  packet_length_tag_key=len_tag_key,
+        	  bps_header=1,
+        	  bps_payload=2,
+        	  debug_log=False,
+        	  scramble_bits=False
+        	 )
+        self.blocks_uchar_to_float_0 = blocks.uchar_to_float()
+        self.blocks_tag_debug_0_0 = blocks.tag_debug(gr.sizeof_char*1, '', ""); self.blocks_tag_debug_0_0.set_display(True)
+        self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_char*1, '/home/supersonic/Desktop/gnuradio_rx/file_rx.txt', True)
+        self.blocks_file_sink_0.set_unbuffered(True)
+
+
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.blocks_uchar_to_float_0, 0), (self.qtgui_sink_x_0, 0))
+        self.connect((self.digital_ofdm_rx_0, 0), (self.blocks_file_sink_0, 0))
+        self.connect((self.digital_ofdm_rx_0, 0), (self.blocks_tag_debug_0_0, 0))
+        self.connect((self.digital_ofdm_rx_0, 0), (self.blocks_uchar_to_float_0, 0))
+        self.connect((self.rational_resampler_xxx_0_0, 0), (self.digital_ofdm_rx_0, 0))
+        self.connect((self.uhd_usrp_source_0, 0), (self.rational_resampler_xxx_0_0, 0))
+
+    def closeEvent(self, event):
+        self.settings = Qt.QSettings("GNU Radio", "rx_ofdm")
+        self.settings.setValue("geometry", self.saveGeometry())
+        event.accept()
+
+    def get_tx_gain(self):
+        return self.tx_gain
+
+    def set_tx_gain(self, tx_gain):
+        self.tx_gain = tx_gain
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.uhd_usrp_source_0.set_samp_rate(self.samp_rate)
+        self.qtgui_sink_x_0.set_frequency_range(2.4e9, self.samp_rate)
+
+    def get_packet_len(self):
+        return self.packet_len
+
+    def set_packet_len(self, packet_len):
+        self.packet_len = packet_len
+
+    def get_len_tag_key(self):
+        return self.len_tag_key
+
+    def set_len_tag_key(self, len_tag_key):
+        self.len_tag_key = len_tag_key
+
+    def get_freq(self):
+        return self.freq
+
+    def set_freq(self, freq):
+        self.freq = freq
+
+    def get_fft_len(self):
+        return self.fft_len
+
+    def set_fft_len(self, fft_len):
+        self.fft_len = fft_len
+
+
+def main(top_block_cls=rx_ofdm, options=None):
+
+    from distutils.version import StrictVersion
+    if StrictVersion(Qt.qVersion()) >= StrictVersion("4.5.0"):
+        style = gr.prefs().get_string('qtgui', 'style', 'raster')
+        Qt.QApplication.setGraphicsSystem(style)
+    qapp = Qt.QApplication(sys.argv)
+
+    tb = top_block_cls()
+    tb.start()
+    tb.show()
+
+    def quitting():
+        tb.stop()
+        tb.wait()
+    qapp.connect(qapp, Qt.SIGNAL("aboutToQuit()"), quitting)
+    qapp.exec_()
+
+
+if __name__ == '__main__':
+    main()
