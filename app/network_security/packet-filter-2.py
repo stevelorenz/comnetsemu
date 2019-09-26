7d84c663134ab2c377938aa1207d83d196c4fe5c
Simon Hanisch <simon.hanisch@tu-dresden.de>
2019-09-26 12:06:51 +0200

    Made packet filter 2 interactive

--- app/network_security/packet-filter-2.py
+++ app/network_security/packet-filter-2.py
@@ -8,6 +8,7 @@ About: Basic example of using Docker as a Mininet host
 
 import comnetsemu.tool as tool
 from time import sleep
+import re
 from comnetsemu.net import Containernet
 from mininet.link import TCLink
 from mininet.log import info, setLogLevel
@@ -26,9 +27,9 @@ def testTopo():
 
     info('*** Adding hosts\n')
     client = net.addDockerHost('client', dimage='sec_test', ip='10.0.0.1',
-                           cpuset_cpus="1", cpu_quota=25000)
+                               cpuset_cpus="1", cpu_quota=25000)
     server = net.addDockerHost('server', dimage='nginx', ip='10.0.0.2',
-                           cpuset_cpus="1", cpu_quota=25000)
+                               cpuset_cpus="1", cpu_quota=25000)
     info('*** Adding switch\n')
     s1 = net.addSwitch('s1')
 
@@ -40,7 +41,7 @@ def testTopo():
     net.start()
 
     info('** client -> server\n')
-    test_connection(client, "10.0.0.2")
+    info('** ' + str(test_connection(client, "10.0.0.2")) + '\n')
 
     info('\n')
 
@@ -52,27 +53,34 @@ def testTopo():
 
     # The server can talk back to client
     info('** server -> client\n')
-    test_connection(server, "10.0.0.2")
+    info('** ' + str(test_connection(server, "10.0.0.1")) + '\n')
     # But he cannot talk to some other server on the internet, this is a problem
     info('** server -> internet\n')
-    test_connection(server, "8.8.8.8")
+    info('** ' + str(test_connection(server, "8.8.8.8")) + '\n')
 
     info('\n')
 
-    info('*** Enable connection tracking\n')
-    #TODO: Create a rule that uses connection tracking to enable hosts on the internet to responde to requests of server
+    info('*** The server can only communicate with the client because of the implemented whitelist filtering.\n')
+    info('*** When the server wants to talk to the internet the internet cannot talk back because the incoming '
+         'traffic is dropped.\n')
+    info('*** Use the connection tracking state to allow established connections to answer the server.\n')
+    info('*** Do this without removing the whitelist.\n')
+
+    while not test_connection(server, "8.8.8.8") or not test_connection(client, "10.0.0.2"):
+        sleep(5)
 
     info('** server -> internet\n')
-    test_connection(server, "8.8.8.8")
+    info('** ' + str(test_connection(server, "8.8.8.8")) + '\n')
+
+    info('*** Now we want to make sure that the client cannot overload the server with traffic.\n')
+    info('*** Drop all traffic of the client that exceeds 10 Mbit/s.\n')
 
     # client is overdoing it a little and our server cannot handle all of its requests...
-    info('*** client is flodding server with too many requests!\n')
+    # info('*** client is flodding server with too many requests!\n')
     server.cmd("iperf -s &")
-    print(client.cmd("iperf -c 10.0.0.2"))
 
-    #TODO: Create a limit rule that allows client to only use a maximum of 10 Mbit/s
-
-    print(client.cmd("iperf -c 10.0.0.2"))
+    while try_to_flood_the_server(client):
+        sleep(5)
 
     info('\n')
 
@@ -85,9 +93,20 @@ def test_connection(source_container, target_ip):
     sent, received = tool.parsePing(ret)
     measured = ((sent - received) / float(sent)) * 100.0
     if measured == 0.0:
-        info('* Connection established\n')
+        return True
     else:
-        info('* Connection denied\n')
+        return False
+
+def try_to_flood_the_server(client):
+    text = client.cmd("iperf -c 10.0.0.2")
+
+    speed = re.search('[0-9]*\.*[0-9]*\sMbits', text)
+    #print(speed)
+    speed = float(speed[0].split(" ")[0])
+    #print(speed)
+
+    return speed > 10.0
+
 
 
 if __name__ == '__main__':
