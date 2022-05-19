5e716110f0ceda5ce8da648c97dc61aadc3d9b24
Zuo Xiang <xianglinks@gmail.com>
2022-05-19 16:00:01 +0200

    Add P4RuntimeSwitch node

--- comnetsemu/node.py
+++ comnetsemu/node.py
@@ -15,8 +15,9 @@ from time import sleep
 import docker
 
 from comnetsemu.exceptions import InvalidDockerArgs
+from comnetsemu.log import debug, error, info, warn, output
 from comnetsemu.util import checkListeningOnPort, dpidToStr
-from mininet.log import debug, error, info, warn
+
 from mininet.moduledeps import pathCheck
 from mininet.node import Host, Switch
 
@@ -388,6 +389,22 @@ class P4DockerHost(DockerHost):
 
         return r
 
+    def describe(self):
+        info("**********\n")
+        output("Network configuration for P4 host: {}\n".format(self.name))
+        output(
+            "Default interface: {}\t IP: {}\t MAC:{}\n".format(
+                self.defaultIntf().name,
+                self.defaultIntf().IP(),
+                self.defaultIntf().MAC(),
+            )
+        )
+        info("**********\n")
+
+
+# TODO:  <19-05-22, Zuo Xiang>
+# 1. Cleanup the classes for P4Switch and P4RuntimeSwitch. Remove duplicated/unnecessary code
+
 
 class P4Switch(Switch):
     """P4 virtual switch"""
@@ -533,3 +550,141 @@ class P4Switch(Switch):
         self.cmd("kill %" + self.sw_path)
         self.cmd("wait")
         self.deleteIntfs()
+
+    def describe(self):
+        info("**********\n")
+        output(
+            "P4RuntimeSwitch name: {}, device_id: {}, dpid:{}, thrift_port: {}, grpc_port: {}\n".format(
+                self.name, self.device_id, self.dpid, self.thrift_port, self.grpc_port
+            )
+        )
+        info("**********\n")
+
+
+class P4RuntimeSwitch(P4Switch):
+    "BMv2 switch with gRPC support"
+    next_grpc_port = 50051
+    next_thrift_port = 9090
+
+    def __init__(
+        self,
+        name,
+        sw_path=None,
+        json_path=None,
+        grpc_port=None,
+        thrift_port=None,
+        pcap_dump=False,
+        log_console=False,
+        verbose=False,
+        device_id=None,
+        enable_debugger=False,
+        log_file=None,
+        **kwargs,
+    ):
+        if device_id is not None:
+            self.device_id = device_id
+            P4Switch.device_id = max(P4Switch.device_id, device_id)
+        else:
+            self.device_id = P4Switch.device_id
+            P4Switch.device_id += 1
+
+        dpid = dpidToStr(self.device_id)
+        kwargs.update(dpid=dpid)
+        Switch.__init__(self, name, **kwargs)
+
+        assert sw_path
+        self.sw_path = sw_path
+        # make sure that the provided sw_path is valid
+        pathCheck(sw_path)
+
+        if json_path is not None:
+            # make sure that the provided JSON file exists
+            if not os.path.isfile(json_path):
+                error("Invalid JSON file: {}\n".format(json_path))
+                exit(1)
+            self.json_path = json_path
+        else:
+            self.json_path = None
+
+        if grpc_port is not None:
+            self.grpc_port = grpc_port
+        else:
+            self.grpc_port = P4RuntimeSwitch.next_grpc_port
+            P4RuntimeSwitch.next_grpc_port += 1
+
+        if thrift_port is not None:
+            self.thrift_port = thrift_port
+        else:
+            self.thrift_port = P4RuntimeSwitch.next_thrift_port
+            P4RuntimeSwitch.next_thrift_port += 1
+
+        if checkListeningOnPort(self.grpc_port):
+            error(
+                "%s cannot bind port %d because it is bound by another process\n"
+                % (self.name, self.grpc_port)
+            )
+            exit(1)
+
+        self.verbose = verbose
+        self.pcap_dump = pcap_dump
+        self.enable_debugger = enable_debugger
+        self.log_console = log_console
+        if log_file is not None:
+            self.log_file = log_file
+        else:
+            self.log_file = "/tmp/p4s.{}.log".format(self.name)
+        self.nanomsg = "ipc:///tmp/bm-{}-log.ipc".format(self.device_id)
+
+    def check_switch_started(self, pid):
+        for _ in range(P4_SWITCH_START_TIMEOUT * 2):
+            if not os.path.exists(os.path.join("/proc", str(pid))):
+                return False
+            if checkListeningOnPort(self.grpc_port):
+                return True
+            sleep(0.5)
+
+    def start(self, controllers=None):
+        info("Starting P4 switch {}.\n".format(self.name))
+        args = [self.sw_path]
+        for port, intf in list(self.intfs.items()):
+            if not intf.IP():
+                args.extend(["-i", str(port) + "@" + intf.name])
+        if self.pcap_dump:
+            args.append("--pcap %s" % self.pcap_dump)
+        if self.nanomsg:
+            args.extend(["--nanolog", self.nanomsg])
+        args.extend(["--device-id", str(self.device_id)])
+        P4Switch.device_id += 1
+        if self.json_path:
+            args.append(self.json_path)
+        else:
+            args.append("--no-p4")
+        if self.enable_debugger:
+            args.append("--debugger")
+        if self.log_console:
+            args.append("--log-console")
+        if self.thrift_port:
+            args.append("--thrift-port " + str(self.thrift_port))
+        if self.grpc_port:
+            args.append("-- --grpc-server-addr 0.0.0.0:" + str(self.grpc_port))
+        cmd = " ".join(args)
+        info(cmd + "\n")
+
+        pid = None
+        with tempfile.NamedTemporaryFile() as f:
+            self.cmd(cmd + " >" + self.log_file + " 2>&1 & echo $! >> " + f.name)
+            pid = int(f.read())
+        debug("P4 switch {} PID is {}.\n".format(self.name, pid))
+        if not self.check_switch_started(pid):
+            error("P4 switch {} did not start correctly.\n".format(self.name))
+            exit(1)
+        info("P4 switch {} has been started.\n".format(self.name))
+
+    def describe(self):
+        info("**********\n")
+        output(
+            "P4RuntimeSwitch name: {}, device_id: {}, dpid:{}, thrift_port: {}, grpc_port: {}\n".format(
+                self.name, self.device_id, self.dpid, self.thrift_port, self.grpc_port
+            )
+        )
+        info("**********\n")
--- examples/p4/single_switch/topology.py
+++ examples/p4/single_switch/topology.py
@@ -6,16 +6,19 @@
 About: A basic example of a BMv2 simple switch (The reference P4 software switch)
 """
 
+import os
 import pathlib
-import subprocess
 import shlex
+import subprocess
 
 from comnetsemu.cli import CLI
 from comnetsemu.log import info, setLogLevel
 from comnetsemu.net import Containernet
-from comnetsemu.node import P4DockerHost, P4Switch
+from comnetsemu.node import P4DockerHost, P4Switch, P4RuntimeSwitch
 from mininet.link import TCLink
 from mininet.node import Controller
+
+CURRENT_DIR = os.path.abspath(os.path.curdir)
 
 
 def buildP4Program():
@@ -40,28 +43,46 @@ def testTopo():
         # xterms=True, spawn xterms for all nodes after net.start()
         net = Containernet(link=TCLink, xterms=True)
 
+        volumes = {CURRENT_DIR: {"bind": "/tmp/single_switch", "mode": "rw"}}
+
         info("*** Adding hosts\n")
-        h1 = net.addDockerHost(
+        h1 = net.addHost(
             "h1",
+            cls=P4DockerHost,
             dimage="dev_test",
             ip="10.0.0.1/24",
             mac="00:04:00:00:00:01",
-            docker_args={"cpuset_cpus": "0", "nano_cpus": int(1e8), "hostname": "h1"},
+            docker_args={
+                "cpuset_cpus": "0",
+                "nano_cpus": int(1e8),
+                "hostname": "h1",
+                "volumes": volumes,
+                "working_dir": "/tmp/single_switch",
+            },
         )
-        h2 = net.addDockerHost(
+        h2 = net.addHost(
             "h2",
+            cls=P4DockerHost,
             dimage="dev_test",
             ip="10.0.0.2/24",
             mac="00:04:00:00:00:02",
-            docker_args={"cpuset_cpus": "0", "nano_cpus": int(1e8), "hostname": "h2"},
+            docker_args={
+                "cpuset_cpus": "0",
+                "nano_cpus": int(1e8),
+                "hostname": "h2",
+                "volumes": volumes,
+                "working_dir": "/tmp/single_switch",
+            },
         )
 
         info("*** Adding P4 switch\n")
         # TODO: Check if options for P4 simple switch can be improved.
-        s1 = net.addP4Switch(
+        s1 = net.addSwitch(
             "s1",
-            sw_path="simple_switch",
+            cls=P4RuntimeSwitch,
+            sw_path="simple_switch_grpc",
             json_path="./build/basic.json",
+            grpc_port=50051,
             thrift_port=9090,
         )
 
@@ -71,6 +92,13 @@ def testTopo():
 
         info("*** Starting network\n")
         net.start()
+
+        info("*** Information of created hosts:\n")
+        for h in net.hosts:
+            h.describe()
+        info("*** Information of created switches:\n")
+        for s in net.switches:
+            s.describe()
 
         info("*** Enter CLI\n")
         info("Use help command to get CLI usages\n")
